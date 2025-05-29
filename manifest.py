7a7398e5c7bd01343493fa9b2333fcdeed7bc569
Nick Budak <thatbudakguy@gmail.com>
2025-05-29 09:20:19 -0700

    Add cross-compiled bytecode

--- manifest.py
+++ manifest.py
@@ -1,2 +1,9 @@
-metadata(version="0.1.0")
-module("thermostat.py")
+metadata(
+  description="Advanced async thermostat logic for micropython",
+  version="0.1.1"
+)
+
+require("time")
+require("logging")
+
+package("thermostat")
